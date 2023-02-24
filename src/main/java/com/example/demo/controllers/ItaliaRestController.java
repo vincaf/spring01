f626be73cbc966149a1078f86b0eea8ab26ea1f3
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2023-02-24 10:09:47 +0100

    feat: add cross origin to ItaliaRestController

--- src/main/java/com/example/demo/controllers/ItaliaRestController.java
+++ src/main/java/com/example/demo/controllers/ItaliaRestController.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ import com.example.demo.services.RegioniService;
 
 @RestController
 @RequestMapping("/italia")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ItaliaRestController {
 	private static final Logger log = LogManager.getLogger();
 	
